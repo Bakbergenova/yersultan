4d3e407cee7afb6d778baa3ff823670f8f7c0404
Aishabibi Bakbergenova <admin@gmail.com>
2025-02-15 17:05:37 +0500

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Git TEST");
+        System.out.println("1. Git TEST");
+        System.out.println("2. Git TEST");
     }
 }
